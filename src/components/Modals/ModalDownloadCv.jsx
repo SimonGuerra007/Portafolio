765f780e86335592766592c0d5588161ceec13e5
SimonGuerra007 <guerrasimon007@gmail.com>
2023-09-28 01:01:37 -0500

    the message to especify the CV language was added

--- src/components/Modals/ModalDownloadCv.jsx
+++ src/components/Modals/ModalDownloadCv.jsx
@@ -5,27 +5,36 @@ const ModalDownloadCv = ({
   darkMode,
   modalDownload,
   handleShowModalDownload,
-  cvLanguage
+  cvLanguage,
 }) => {
   const { t } = useTranslation();
   return (
     <div
-      className={`h-screen w-full bg-black/60 flex justify-center items-center fixed top-0  z-[7500] ${modalDownload ? 'visible' : 'invisible'} `}
+      className={`h-screen w-full bg-black/60 flex justify-center items-center fixed top-0  z-[7500] ${
+        modalDownload ? "visible" : "invisible"
+      } `}
     >
       <div
-        className={`w-[100%] h-[50%] sm:w-[60%] sm:h-[50%] p-5 rounded-[20px] flex flex-col justify-center items-center gap-[30px] transition-all fixed ${modalDownload ? 'sm:left-[20%] left-0' : 'left-[-200%]'} ${
+        className={`w-[100%] h-[50%] sm:w-[60%] sm:h-[50%] p-5 rounded-[20px] flex flex-col justify-center items-center gap-[30px] transition-all fixed ${
+          modalDownload ? "sm:left-[20%] left-0" : "left-[-200%]"
+        } ${
           darkMode
             ? "bg-gradient-to-r from-[#161616] to-[#3f3f3f]"
             : "bg-gradient-to-r from-[#64afff] to-[#e3e3e3]"
         }`}
       >
-        <h4 className="text-2xl text-center font-semibold">
-          {t("modalDownload.message")}
-        </h4>
+        <h3 className="text-2xl text-center font-semibold">
+          {t("modalDownload.title")}
+        </h3>
+        <p className="text-center text-[14px] text-yellow-500">{t("modalDownload.message")}</p>
         <div className="flex justify-center items-center gap-[30px]">
           <a
             onClick={handleShowModalDownload}
-            href={cvLanguage ? "/files/CVSimonGuerra_Español.pdf" : "/files/CVSimonGuerra_English.pdf"}
+            href={
+              cvLanguage
+                ? "/files/CVSimonGuerra_Español.pdf"
+                : "/files/CVSimonGuerra_English.pdf"
+            }
             download="CV Simon David Guerra Bedoya"
             className={`w-[100px] py-2 shadow-md rounded-[5px] text-center font-semibold transition-all bg-[#cf9013] hover:bg-[#ffb218] ${
               darkMode
