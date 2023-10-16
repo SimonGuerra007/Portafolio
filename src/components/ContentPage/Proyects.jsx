e67a166e6345f83a6802dde54c2ba0ae8d4e69bb
SimonGuerra007 <guerrasimon007@gmail.com>
2023-10-15 19:29:05 -0500

    the project's name was added

--- src/components/ContentPage/Proyects.jsx
+++ src/components/ContentPage/Proyects.jsx
@@ -23,50 +23,142 @@ const Proyects = ({ darkMode }) => {
       <div className="flex flex-col sm:flex-row gap-[20px] lg:gap-[60px]">
         <div className="flex flex-col gap-[20px] lg:gap-[60px]">
           <div className="w-[230px] sm:w-[280px] lg:w-[350px] relative">
-            <div className="hover:bg-black/80 rounded-[10px] absolute opacity-0 hover:opacity-[1] transition-all h-full w-full cursor-pointer flex flex-col sm:flex-row justify-center items-center gap-[10px] lg:gap-[30px]">
-              <a href="https://pokedex-simonguerra.netlify.app/pokedex" target="blank" className={` rounded-[5px] py-[5px] px-[20px] transition-all font-semibold ${darkMode ? 'bg-[#ffb92c] hover:bg-[#c48e31]' : 'bg-[#1e8bff] hover:bg-[#5fadff]'}`}>
-                {t("projects.seeWeb")}
-              </a>
-              <a href="https://github.com/SimonGuerra007/Pokedex" target="blank" className={` rounded-[5px] py-[5px] px-[20px] transition-all font-semibold ${darkMode ? 'bg-[#ffb92c] hover:bg-[#c48e31]' : 'bg-[#1e8bff] hover:bg-[#5fadff]'}`}>
-                {t("projects.seeRepository")}
-              </a>
+            <div className="hover:bg-black/80 rounded-[10px] absolute opacity-0 hover:opacity-[1] transition-all h-full w-full cursor-pointer flex flex-col justify-center items-center gap-4">
+              <h3 className="font-semibold text-3xl text-white">Pokedex</h3>
+              <div className="flex flex-col sm:flex-row justify-center items-center gap-[10px] lg:gap-[30px]">
+                <a
+                  href="https://pokedex-simonguerra.netlify.app/pokedex"
+                  target="blank"
+                  className={` rounded-[5px] py-[5px] px-[20px] transition-all font-semibold ${
+                    darkMode
+                      ? "bg-[#ffb92c] hover:bg-[#c48e31]"
+                      : "bg-[#1e8bff] hover:bg-[#5fadff]"
+                  }`}
+                >
+                  {t("projects.seeWeb")}
+                </a>
+                <a
+                  href="https://github.com/SimonGuerra007/Pokedex"
+                  target="blank"
+                  className={` rounded-[5px] py-[5px] px-[20px] transition-all font-semibold ${
+                    darkMode
+                      ? "bg-[#ffb92c] hover:bg-[#c48e31]"
+                      : "bg-[#1e8bff] hover:bg-[#5fadff]"
+                  }`}
+                >
+                  {t("projects.seeRepository")}
+                </a>
+              </div>
             </div>
             <img className="rounded-[10px]" src="/images/Pokedex.png" alt="" />
           </div>
           <div className="w-[230px] sm:w-[280px] lg:w-[350px] relative">
-            <div className="hover:bg-black/80 rounded-[10px] absolute opacity-0 hover:opacity-[1] transition-all h-full w-full cursor-pointer flex flex-col sm:flex-row justify-center items-center gap-[10px] lg:gap-[30px]">
-              <a href="https://weather-app-simonguerra.netlify.app/" target="blank" className={` rounded-[5px] py-[5px] px-[20px] transition-all font-semibold ${darkMode ? 'bg-[#ffb92c] hover:bg-[#c48e31]' : 'bg-[#1e8bff] hover:bg-[#5fadff]'}`}>
-                {t("projects.seeWeb")}
-              </a>
-              <a href="https://github.com/SimonGuerra007/Weather-App" target="blank" className={` rounded-[5px] py-[5px] px-[20px] transition-all font-semibold ${darkMode ? 'bg-[#ffb92c] hover:bg-[#c48e31]' : 'bg-[#1e8bff] hover:bg-[#5fadff]'}`}>
-                {t("projects.seeRepository")}
-              </a>
+            <div className="hover:bg-black/80 rounded-[10px] absolute opacity-0 hover:opacity-[1] transition-all h-full w-full cursor-pointer flex flex-col justify-center items-center gap-4">
+              <h3 className="text-white text-3xl font-semibold">
+                Proceso Seguro
+              </h3>
+              <div className="flex flex-col sm:flex-row justify-center items-center gap-[10px] lg:gap-[30px]">
+                <a
+                  href="https://erp-ps-mauricio.web.app/"
+                  target="blank"
+                  className={` rounded-[5px] py-[5px] px-[20px] transition-all font-semibold ${
+                    darkMode
+                      ? "bg-[#ffb92c] hover:bg-[#c48e31]"
+                      : "bg-[#1e8bff] hover:bg-[#5fadff]"
+                  }`}
+                >
+                  {t("projects.seeWeb")}
+                </a>
+                <a
+                  href="https://github.com/SimonGuerra007/Weather-App"
+                  target="blank"
+                  className={` rounded-[5px] py-[5px] px-[20px] transition-all font-semibold ${
+                    darkMode
+                      ? "bg-[#ffb92c] hover:bg-[#c48e31]"
+                      : "bg-[#1e8bff] hover:bg-[#5fadff]"
+                  }`}
+                >
+                  {t("projects.seeRepository")}
+                </a>
+              </div>
             </div>
-            <img className="rounded-[10px]" src="/images/Weather.png" alt="" />
+            <img
+              className="rounded-[10px]"
+              src="/images/ProcesoSeguro.png"
+              alt=""
+            />
           </div>
         </div>
         <div className="flex flex-col gap-[20px] lg:gap-[60px]">
           <div className="w-[230px] sm:w-[280px] lg:w-[350px] relative">
-            <div className="hover:bg-black/80 rounded-[10px] absolute opacity-0 hover:opacity-[1] transition-all h-full w-full cursor-pointer flex flex-col sm:flex-row justify-center items-center gap-[10px] lg:gap-[30px]">
-              <a href="https://rickandmorty-simonguerra.netlify.app/" target="blank" className={` rounded-[5px] py-[5px] px-[20px] transition-all font-semibold ${darkMode ? 'bg-[#ffb92c] hover:bg-[#c48e31]' : 'bg-[#1e8bff] hover:bg-[#5fadff]'}`}>
-                {t("projects.seeWeb")}
-              </a>
-              <a href="https://github.com/SimonGuerra007/RickAndMorty" target="blank" className={` rounded-[5px] py-[5px] px-[20px] transition-all font-semibold ${darkMode ? 'bg-[#ffb92c] hover:bg-[#c48e31]' : 'bg-[#1e8bff] hover:bg-[#5fadff]'}`}>
-                {t("projects.seeRepository")}
-              </a>
+            <div className="hover:bg-black/80 rounded-[10px] absolute opacity-0 hover:opacity-[1] transition-all h-full w-full cursor-pointer flex flex-col justify-center items-center gap-4">
+              <h3 className="text-white text-3xl font-semibold">
+                Rick And Morty
+              </h3>
+              <div className="flex flex-col sm:flex-row justify-center items-center gap-[10px] lg:gap-[30px]">
+                <a
+                  href="https://rickandmorty-simonguerra.netlify.app/"
+                  target="blank"
+                  className={` rounded-[5px] py-[5px] px-[20px] transition-all font-semibold ${
+                    darkMode
+                      ? "bg-[#ffb92c] hover:bg-[#c48e31]"
+                      : "bg-[#1e8bff] hover:bg-[#5fadff]"
+                  }`}
+                >
+                  {t("projects.seeWeb")}
+                </a>
+                <a
+                  href="https://github.com/SimonGuerra007/RickAndMorty"
+                  target="blank"
+                  className={` rounded-[5px] py-[5px] px-[20px] transition-all font-semibold ${
+                    darkMode
+                      ? "bg-[#ffb92c] hover:bg-[#c48e31]"
+                      : "bg-[#1e8bff] hover:bg-[#5fadff]"
+                  }`}
+                >
+                  {t("projects.seeRepository")}
+                </a>
+              </div>
             </div>
-            <img className="rounded-[10px]" src="/images/RickAndMorty.png" alt="" />
+            <img
+              className="rounded-[10px]"
+              src="/images/RickAndMorty.png"
+              alt=""
+            />
           </div>
           <div className="w-[230px] sm:w-[280px] lg:w-[350px] relative">
-            <div className="hover:bg-black/80 rounded-[10px] absolute opacity-0 hover:opacity-[1] transition-all h-full w-full cursor-pointer flex flex-col sm:flex-row justify-center items-center gap-[10px] lg:gap-[30px]">
-              <a href="https://ecommerce-simonguerra.netlify.app/" target="blank" className={` rounded-[5px] py-[5px] px-[20px] transition-all font-semibold ${darkMode ? 'bg-[#ffb92c] hover:bg-[#c48e31]' : 'bg-[#1e8bff] hover:bg-[#5fadff]'}`}>
-                {t("projects.seeWeb")}
-              </a>
-              <a href="https://github.com/SimonGuerra007/ecommerce" target="blank" className={` rounded-[5px] py-[5px] px-[20px] transition-all font-semibold ${darkMode ? 'bg-[#ffb92c] hover:bg-[#c48e31]' : 'bg-[#1e8bff] hover:bg-[#5fadff]'}`}>
-                {t("projects.seeRepository")}
-              </a>
+            <div className="hover:bg-black/80 rounded-[10px] absolute opacity-0 hover:opacity-[1] transition-all h-full w-full cursor-pointer flex flex-col justify-center items-center gap-4">
+              <h3 className="text-white text-3xl font-semibold">Ecommerce</h3>
+              <div className="flex flex-col sm:flex-row justify-center items-center gap-[10px] lg:gap-[30px]">
+                <a
+                  href="https://ecommerce-simonguerra.netlify.app/"
+                  target="blank"
+                  className={` rounded-[5px] py-[5px] px-[20px] transition-all font-semibold ${
+                    darkMode
+                      ? "bg-[#ffb92c] hover:bg-[#c48e31]"
+                      : "bg-[#1e8bff] hover:bg-[#5fadff]"
+                  }`}
+                >
+                  {t("projects.seeWeb")}
+                </a>
+                <a
+                  href="https://github.com/SimonGuerra007/ecommerce"
+                  target="blank"
+                  className={` rounded-[5px] py-[5px] px-[20px] transition-all font-semibold ${
+                    darkMode
+                      ? "bg-[#ffb92c] hover:bg-[#c48e31]"
+                      : "bg-[#1e8bff] hover:bg-[#5fadff]"
+                  }`}
+                >
+                  {t("projects.seeRepository")}
+                </a>
+              </div>
             </div>
-            <img className="rounded-[10px]" src="/images/Ecommerce.png" alt="" />
+            <img
+              className="rounded-[10px]"
+              src="/images/Ecommerce.png"
+              alt=""
+            />
           </div>
         </div>
       </div>
